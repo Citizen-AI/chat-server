db35406f53efef65883db9056bba26264d6f5903
Matthew Bartlett <mhjbnz@gmail.com>
2020-05-15 15:37:27 +1200

    rearrange contents page

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ bus
   .on('message from Web Adapter user', logger.from_user)
   .on('postback from Web Adapter: tell me more', web.tell_me_more)
   .on('postback from Web Adapter: tell me more', logger.from_user)
-  .on('message to Web Adapter user', logger.to_user)
 
   .on('Facebook Adapter user starts', df.send_to_df)
   .on('Facebook Adapter user starts', logger.user_starts)
@@ -27,12 +26,14 @@ bus
   .on('postback from Facebook Adapter: card button', df.send_to_df)
   .on('postback from Facebook Adapter: tell me more', fb.tell_me_more)
   .on('postback from Facebook Adapter: tell me more', logger.from_user)
-  .on('message to Facebook Adapter user', logger.to_user)
 
   .on('message from Dialogflow for Web Adapter', web.send_queue)
   .on('message from Dialogflow for Facebook Adapter', fb.send_queue)
   .on('message from Dialogflow for Web Adapter', logger.from_df)
   .on('message from Dialogflow for Facebook Adapter', logger.from_df)
+
+  .on('message to Web Adapter user', logger.to_user)
+  .on('message to Facebook Adapter user', logger.to_user)
 
   .on('user feedback received', logger.feedback)
 
